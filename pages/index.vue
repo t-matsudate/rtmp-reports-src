ee54b660abd8895727da53ba6111c1ae18829c4b
Tsuyoshi Matsudate <t.matsudate@gmail.com>
2020-02-13 18:42:08 +0900

    1 Added, 1 Changed.
    Added:
    
    * A menu of the new article.
    
    Changed:
    
    * Publshed/Modified date has been updated semi-automatically.

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <article id="index">
-    <Author :author="author" :published="published" :modified="modified" />
+    <Author :author="author" :published="firstPublished" :modified="lastModified" />
     <ReportTitle :report-title="title" />
     <ol>
       <li>
         <h2><nuxt-link to="/articles/overview">RTMP の概要</nuxt-link></h2>
         <ul class="datetime">
-          <li class="modified">更新日: <time :datetime="modified">{{ modified }}</time></li>
-          <li class="published">投稿日: <time :datetime="published">{{ published }}</time></li>
+          <li class="modified">更新日: <time :datetime="modified.overview">{{ modified.overview }}</time></li>
+          <li class="published">投稿日: <time :datetime="published.overview">{{ published.overview }}</time></li>
         </ul>
         <p>記載内容：</p>
         <ul class="contents">
@@ -15,6 +15,19 @@
           <li>基本的な通信手順について.</li>
           <li>既存製品はどのように通信しているか.</li>
           <li>私達はそれらの製品とどのように通信していけばよいか.</li>
+        </ul>
+      </li>
+      <li>
+        <h2><nuxt-link to="/articles/connection-implementation">ハンドシェイクと Invoke 処理の実装</nuxt-link></h2>
+        <ul class="datetime">
+          <li class="published">投稿日: <time :datetime="published.connectionImplementation">{{ published.connectionImplementation }}</time></li>
+        </ul>
+        <p>記載内容：</p>
+        <ul class="contents">
+          <li>RTMP ハンドシェイクの実装.</li>
+          <li>connect 呼び出しの処理方法について.</li>
+          <li>releaseStream, onFCPublish および createStream 呼び出しの処理方法について.</li>
+          <li>publish 呼び出しの処理方法について.</li>
         </ul>
       </li>
     </ol>
@@ -38,9 +51,22 @@ export default {
       title: 'RTMP Implementation Reports',
       description: 'RTMP サーバの実装メモと直近の仕様の整理.',
       author: 'T.Matsudate',
-      published: '2019-09-09',
-      modified: '2020-01-04'
+      published: {
+        overview: '2019-09-09',
+        connectionImplementation: '2020-02-13'
+      },
+      modified: {
+        overview: '2020-01-04'
+      },
     }
+  },
+  computed: {
+    firstPublished() {
+      return Object.values(this.published).reduce((acc, val) => new Date(acc) < new Date(val) ? acc : val)
+    },
+    lastModified() {
+      return Object.values(this.published).concat(Object.values(this.modified)).reduce((acc, val) => new Date(acc) > new Date(val) ? acc : val)
+    },
   },
   head() {
     return {
